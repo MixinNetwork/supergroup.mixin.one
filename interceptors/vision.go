cac8cfcbe8e79d72a5eab21368448fc6c03af9ce
Li Yuqing <im.yuqlee@gmail.com>
2019-04-15 00:02:27 +0800

    More log for Adult

--- interceptors/vision.go
+++ interceptors/vision.go
@@ -4,18 +4,21 @@ import (
 	"context"
 
 	vision "cloud.google.com/go/vision/apiv1"
+	"github.com/MixinNetwork/supergroup.mixin.one/session"
 	pp "google.golang.org/genproto/googleapis/cloud/vision/v1"
 )
 
 func CheckSex(ctx context.Context, uri string) bool {
 	client, err := vision.NewImageAnnotatorClient(ctx)
 	if err != nil {
-		return true
+		session.Logger(ctx).Errorf("CheckSex NewImageAnnotatorClient ERROR: %+v", err)
+		return false
 	}
 	image := vision.NewImageFromURI(uri)
 	safe, err := client.DetectSafeSearch(ctx, image, nil)
 	if err != nil {
-		return true
+		session.Logger(ctx).Errorf("CheckSex DetectSafeSearch ERROR: %+v", err)
+		return false
 	}
 	if safe.Adult >= pp.Likelihood_LIKELY {
 		return true
