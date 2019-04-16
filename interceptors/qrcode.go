cbdac16b39def03fda3787670d18ecd05053337d
Li Yuqing <im.yuqlee@gmail.com>
2019-04-16 15:33:56 +0800

    Fix QRcode detector

--- interceptors/qrcode.go
+++ interceptors/qrcode.go
@@ -3,12 +3,14 @@ package interceptors
 import (
 	"context"
 	"net/http"
+	"strings"
 	"time"
 
+	"github.com/MixinNetwork/supergroup.mixin.one/session"
 	"github.com/tuotoo/qrcode"
 )
 
-func CheckQRCode(uri string) bool {
+func CheckQRCode(ctx context.Context, uri string) bool {
 	req, err := http.NewRequest(http.MethodGet, uri, nil)
 	if err != nil {
 		return false
@@ -18,12 +20,18 @@ func CheckQRCode(uri string) bool {
 	defer cancel()
 	resp, _ := http.DefaultClient.Do(req.WithContext(ctx))
 	if err != nil {
+		session.Logger(ctx).Errorf("CheckQRCode ERROR: %+v", err)
 		return false
 	}
 	defer resp.Body.Close()
 
 	qrmatrix, err := qrcode.Decode(resp.Body)
 	if err != nil {
+		session.Logger(ctx).Errorf("CheckQRCode Decode ERROR: %+v", err)
+		if strings.Contains(err.Error(), "level and mask") {
+			return true
+		}
+
 		return false
 	}
 	if len(qrmatrix.Content) > 0 {
