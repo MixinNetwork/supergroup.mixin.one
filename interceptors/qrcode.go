c992ab16b4aced90dd638013ca76addfeb3f0edc
Li Yuqing <im.yuqlee@gmail.com>
2019-04-20 22:21:30 +0800

    Remove level and mask error

--- interceptors/qrcode.go
+++ interceptors/qrcode.go
@@ -3,7 +3,6 @@ package interceptors
 import (
 	"bytes"
 	"context"
-	"strings"
 
 	"github.com/MixinNetwork/supergroup.mixin.one/session"
 	"github.com/tuotoo/qrcode"
@@ -13,10 +12,6 @@ func CheckQRCode(ctx context.Context, data []byte) (bool, error) {
 	qrmatrix, err := qrcode.Decode(bytes.NewReader(data))
 	if err != nil {
 		session.Logger(ctx).Errorf("CheckQRCode Decode ERROR: %+v", err)
-		if strings.Contains(err.Error(), "level and mask") {
-			return true, err
-		}
-
 		return false, err
 	}
 	session.Logger(ctx).Infof("CheckQRCode qrmatrix: %d", len(qrmatrix.Content))
