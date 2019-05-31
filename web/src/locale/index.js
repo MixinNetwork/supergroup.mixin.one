14b2a77c74fc5f18397d874a84089d382b0ae9b3
Wei <5h3ll3x@gmail.com>
2019-05-31 11:06:29 +0800

    fix locale issue

--- web/src/locale/index.js
+++ web/src/locale/index.js
@@ -1,9 +1,15 @@
 import Polyglot from 'node-polyglot';
+const en = require('./en-US.json');
+const zh = require('./zh-CN.json');
 
 function Locale(lang) {
+  var localeMap = {
+    "en-US": require('./en-US.json'),
+    "zh-CN": require('./zh-CN.json'),
+  }
   var locale = LOCALE;
   this.polyglot = new Polyglot({locale: locale});
-  this.polyglot.extend(require('./' + locale + '.json'));
+  this.polyglot.extend(localeMap[locale]);
 }
 
 Locale.prototype = {
