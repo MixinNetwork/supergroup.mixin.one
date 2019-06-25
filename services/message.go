fbe5caeedb356af8cfc12bae400c8427d87590fe
Yuqing Lee <im.yuqlee@gmail.com>
2019-06-25 22:51:00 +0800

    Delete unused code

--- services/message.go
+++ services/message.go
@@ -350,15 +350,8 @@ func handleTransfer(ctx context.Context, mc *MessageContext, transfer TransferVi
 			return user.Payment(ctx)
 		}
 		for _, asset := range config.Get().System.AccpetPaymentAssetList {
-			if asset.Amount != "auto" {
-				if transfer.Amount == asset.Amount && transfer.AssetId == asset.AssetId {
-					return user.Payment(ctx)
-				}
-			} else if config.Get().System.AutoEstimate {
-				// @TODO
-				// if abs(float(config.Get().System.AutoEstimateBase) - models.EstimateUsd(transfer.Amount)) < 0.1 && transfer.AssetId == asset.AssetId {
-				// 	return user.Payment(ctx)
-				// }
+			if transfer.Amount == asset.Amount && transfer.AssetId == asset.AssetId {
+				return user.Payment(ctx)
 			}
 		}
 	} else if packet, err := models.PayPacket(ctx, id.String(), transfer.AssetId, transfer.Amount); err != nil || packet == nil {
