a6ab0a76da6f4079fd34c83ac70e67fe027f02bf
Li Yuqing <im.yuqlee@gmail.com>
2019-06-25 21:10:56 +0800

    Bugfix: limiter

--- durable/limiter.go
+++ durable/limiter.go
@@ -10,7 +10,7 @@ var limiters map[string]*rate.Limiter = make(map[string]*rate.Limiter, 0)
 
 func Allow(key string) bool {
 	if limiters[key] == nil {
-		limiters[key] = rate.NewLimiter(rate.Every(30*time.Second), 10)
+		limiters[key] = rate.NewLimiter(rate.Every(3*time.Second), 10)
 	}
 	return limiters[key].Allow()
 }
