97835bb6f36127e2f7945cf073781cdbcc725b1e
Wei <5h3ll3x@gmail.com>
2019-06-25 14:43:20 +0800

    Remove toast when oauth begins

--- new-web/src/pages/TestAuth.vue
+++ new-web/src/pages/TestAuth.vue
@@ -8,15 +8,13 @@ import { Toast } from 'vant'
 export default {
   async mounted() {
     const code = this.$route.query.code
-    Toast('Loading')
     try {
       let resp = await this.GLOBAL.api.account.authenticate(code)
       if (resp.data.authentication_token) {
         this.$router.push('/')
       }
     } catch (err) {
-      console.log(err)
-      Toast('Failed to Authorize')
+      Toast('OAuth Failed')
       this.$router.push('/')
     }
   }
