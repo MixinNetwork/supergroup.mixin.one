1778fa01b6b75455c0ba822c7027d56a4ab5095a
Eric Zhang <i@qinix.com>
2019-07-12 03:00:46 +0800

    (plugin): support web shortcuts

--- plugins/hello/main.go
+++ plugins/hello/main.go
@@ -28,6 +28,10 @@ func PluginInit(plugCtx *plugin.PluginContext) {
 	r.GET("/hello/world", helloWorld)
 
 	pluginContext.RegisterHTTPHandler("hello", r) //nolint:errcheck
+
+	group := plugin.Shortcut.CreateGroup("hello", "Hello", "你好", 0)
+	group.CreateItem("test2", "Test2", "测试2", "https://mixin.one", 1)
+	group.CreateItem("test1", "Test1", "测试1", "https://mixin.one", 0)
 }
 
 func helloWorld(c *gin.Context) {
