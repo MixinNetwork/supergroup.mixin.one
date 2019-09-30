80987a64cbcfe5d91ccf7050ba9c7decb8f1103d
Li Yuqing <im.yuqlee@gmail.com>
2019-09-30 19:26:49 +0800

    System message data shouldn't blank

--- models/distributed_message.go
+++ models/distributed_message.go
@@ -248,6 +248,9 @@ func (message *Message) Leapfrog(ctx context.Context, reason string) error {
 }
 
 func createSystemDistributedMessage(ctx context.Context, user *User, category, data string) error {
+	if len(data) == 0 {
+		return nil
+	}
 	dm, err := createDistributeMessage(ctx, bot.UuidNewV4().String(), bot.UuidNewV4().String(), "", config.AppConfig.Mixin.ClientId, user.UserId, MessageCategoryPlainText, data)
 	if err != nil {
 		return session.TransactionError(ctx, err)
